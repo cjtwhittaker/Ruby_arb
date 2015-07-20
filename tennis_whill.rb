78b4a1b2019b12e0a181a566fd56d02b0b0e5128
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-07-20 15:07:06 +0100

    updated william hill tennis

--- tennis_whill.rb
+++ tennis_whill.rb
@@ -1,13 +1,49 @@
-def tennis_whill
-  @browser.goto "http://sports.williamhill.com/bet/en-gb/betting/y/17/mh/Tennis.html"
+def tennis_whill_regexp
+  @whill_browser_tennis.goto 'http://sports.williamhill.com/bet/en-gb/betting/y/17/mh/Tennis.html'
+  whill_tennis_data = @whill_browser_tennis.text
+  whill_tennis_matches = whill_tennis_data.scan(/(?:(\d{2}:\d{2})\s\w{2}\n)?(\d+\/\d+)\n(.+)\s{3}v\s{3}(.+)\n(\d+\/\d+)/)
+  whill_tennis_matches_decimal = []
+  whill_tennis_matches.each do |z|
+    whill_tennis_matches_decimal << [z[0], oddconverter(z[1]), z[2], z[3], oddconverter(z[4])]
+  end
+  return whill_tennis_matches_decimal
+end
 
-  file = File.open 'c:/users/charlie/documents/tennis.txt', 'w'
-  file << @browser.text
-  file.close
-  file = File.open 'c:/users/charlie/documents/tennis.txt', 'r'
-  data = file.read
 
-  player = data.match(/(^\w+-?.?\w*\s?\w*)(?:\s{3}v)/)[1]
+def tennis_whill_css
+  whill_tennis_url = 'http://sports.williamhill.com/bet/en-gb/betting/y/17/mh/Tennis.html'
+  unless @whill_browser_tennis.url == whill_tennis_url
+    @whill_browser_tennis.goto whill_tennis_url
+  end
+  @whill_tennis_match = []
+  i = 0
+  @whill_browser_tennis.trs(class:'rowOdd').each do |row|
+    players = row[4].text.match(/(.*)\s\s\sv\s\s\s(.*)/)
+    @whill_tennis_odds_a = oddconverter row[3].text
+    @whill_tennis_odds_b = oddconverter row[5].text
+    puts i 
+    i = i + 1
+    @whill_tennis_match << [row[1].text.match(/\d{2}:\d{2}/)[0], @whill_tennis_odds_a, players[1], players[2], @whill_tennis_odds_b]
+
+    begin
+      rescue Exception => e    
+    end
+
+  end 
+end
+
+
+
+
+
+
+  # file = File.open 'c:/users/charlie/documents/tennis.txt', 'w'
+  # file << @browser.text
+  # file.close
+  # file = File.open 'c:/users/charlie/documents/tennis.txt', 'r'
+  # data = file.read
+
+  # player = data.match(/(^\w+-?.?\w*\s?\w*)(?:\s{3}v)/)[1]
 
 
 
@@ -20,8 +56,8 @@ def tennis_whill
 # array with all the tennis players
  
 #then split the players into seperate arrays
-  player_1 = []
-  player_2 = []
+  # player_1 = []
+  # player_2 = []
 
 
 
@@ -36,12 +72,8 @@ def tennis_whill
 
 #take the odds for each player
 
-end
-
 
 #getting the data into the file 
-@whill_tennis_browser.goto 'http://sports.williamhill.com/bet/en-gb/betting/y/17/mh/Tennis.html'
-whill_tennis_data = @browser.text
 #file = File.open 'c:/users/charlie/documents/tennis.txt', 'w'
 #file << whill_tennis_data
 
@@ -52,24 +84,23 @@ whill_tennis_data = @browser.text
 #returns non live results
 #need to add doubles to the regular expression
 
-@whill_tennis_match = whill_tennis_data.scan(/(\d{2}:\d{2}\s..|(?:Live\n\d+-\d+))\n(\d+\/\d+)\n(^\w+-?.?\w*\s?\w*)\s{3}v\s{3}(\w+-?\.?\w*\s?\w*)\n(\d+\/\d+)/)
-whill_tennis_time = []
-whill_tennis_odds_a = []
-whill_tennis_player_a = []
-whill_tennis_odds_b = []
-whill_tennis_player_b = []
+# whill_tennis_time = []
+# whill_tennis_odds_a = []
+# whill_tennis_player_a = []
+# whill_tennis_odds_b = []
+# whill_tennis_player_b = []
 
-whill_regex_splitter(whill_tennis_time,0)
-whill_regex_splitter(whill_tennis_odds_a,1)
-whill_regex_splitter(whill_tennis_player_a,2)
-whill_regex_splitter(whill_tennis_odds_b,3)
-whill_regex_splitter(whill_tennis_player_b,4)
+# whill_regex_splitter(whill_tennis_time,0)
+# whill_regex_splitter(whill_tennis_odds_a,1)
+# whill_regex_splitter(whill_tennis_player_a,2)
+# whill_regex_splitter(whill_tennis_odds_b,3)
+# whill_regex_splitter(whill_tennis_player_b,4)
 
-def whill_regex_splitter(output_arr, output_index)
-  output_arr = []
-  @whill_tennis_match.each do |split|
-    output_arr << split[output_index]
-  end
-end
+# def whill_regex_splitter(output_arr, output_index)
+#   output_arr = []
+#   @whill_tennis_match.each do |split|
+#     output_arr << split[output_index]
+#   end
+# end
 
 

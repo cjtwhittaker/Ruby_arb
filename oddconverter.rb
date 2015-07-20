310bf3bf80f2b0f81ac6207309d1fa88b48d6df8
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-07-20 15:04:57 +0100

    updated oddconverter

--- oddconverter.rb
+++ oddconverter.rb
@@ -1,6 +1,10 @@
 def oddconverter(odd)
-  numerator = odd[/\d*/].to_f
-  denominator = odd[/\d*$/].to_f
-  decimal = (numerator / denominator) + 1
-  return decimal
+  if odd == 'EVS'
+    return 2.0
+  else
+    numerator = odd[/\d*/].to_f
+    denominator = odd[/\d*$/].to_f
+    decimal = (numerator / denominator) + 1
+    return decimal
+  end
 end
